1a35b7294e3d1d6e8d6699605ae3d1d73c791284
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2025-02-08 14:24:22 +0200

    Stripe payment and verify API using

--- base/views.py
+++ base/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required 
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+from django.http import JsonResponse
+import stripe
 from base import models as base_models
 from doctor import models as doctor_models
 from patient import models as patient_models
@@ -88,4 +92,72 @@ def checkout(request, billing_id):
 
     }
     return render(request, "base/checkout.html", context)
+
+
+@csrf_exempt
+def stripe_payment(request, billing_id):
+    billing = base_models.Billing.objects.get(billing_id=billing_id)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    checkout_session = stripe.checkout.Session.create(
+        customer_email=billing.patient.email,
+        payment_method_types=['card'],
+        line_items = [
+            {
+                'price_data': {
+                    'currency': 'USD',
+                    'product_data': {
+                        'name': billing.patient.full_name
+                    },
+                    'unit_amount': int(billing.total * 100)
+                },
+                'quantity': 1
+            }
+        ],
+        mode='payment',
+        success_url = request.build_absolute_uri(reverse("base:stripe_payment_verify", args=[billing.billing_id])) + "?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url=request.build_absolute_uri(reverse("base:stripe_payment_verify", args=[billing.billing_id])) + "?session_id={CHECKOUT_SESSION_ID}"
         
+    )
+    return JsonResponse({"sessionId": checkout_session.id})
+
+
+def stripe_payment_verify(request, billing_id):
+    billing = base_models.Billing.objects.get(billing_id=billing_id)
+    session_id = request.GET.get("session_id")
+    session = stripe.checkout.Session.retrieve(session_id)
+
+    if session.payment_status == "paid":
+        if billing.status == "Unpaid":
+            billing.status = "Paid"
+            billing.save()
+            billing.appointment.status = "Completed"
+            billing.appointment.save()
+
+            doctor_models.Notification.objects.create(
+                doctor=billing.appointment.doctor,
+                appointment=billing.appointment,
+                type="New Appointment"
+            )
+
+            patient_models.Notification.objects.create(
+                patient=billing.appointment.patient,
+                appointment=billing.appointment,
+                type="Appointment Scheduled"
+            )
+
+            return redirect(f"/payment_status/{billing.billing_id}/?payment_status=paid")
+    else:
+        return redirect(f"/payment_status/{billing.billing_id}/?payment_status=failed")
+    
+    
+@login_required
+def payment_status(request, billing_id):
+    billing = base_models.Billing.objects.get(billing_id=billing_id)
+    payment_status = request.GET.get("payment_status")
+
+    context = {
+        "billing": billing,
+        "payment_status": payment_status,
+    }
+    return render(request, "base/payment_status.html", context)
